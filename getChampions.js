57a2a9065242b4afa5bd2e36ecd974c566f9930a
Connor Vinchoff ConnorVin <43933461+ConnorVin@users.noreply.github.com>
2019-04-25 11:12:49 -0700

    Created server.js and getChampions.js

---
+++ getChampions.js
@@ -0,0 +1,27 @@
+const http = require('http');
+var url = "http://ddragon.leagueoflegends.com/cdn/9.8.1/data/en_US/champion.json";
+
+// Returns a list of champion objects that contains basic info
+exports.getChampions = (callback) => {
+    const req = http.request(url, (res) => {
+        var data = '';
+
+        // res is a readable stream so we collect its information through the 'data' event
+        res.on('data', chunk => {
+            data += chunk;
+        });
+    
+        // When there's no more data being sent, then begin manipulating it
+        res.on('end', () => {
+            data = JSON.parse(data);
+            callback(Object.values(data.data));
+        });
+    });
+    
+    req.on('error', err => {
+        console.log(err);
+    });
+    
+    req.end();
+}
+
