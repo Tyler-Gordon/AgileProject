7cb8b553849dc451aea19875db259b0c1daa9626
Connor Vinchoff ConnorVin <43933461+ConnorVin@users.noreply.github.com>
2019-04-30 18:25:34 -0700

    Added /calculate endpoint and modified getData

---
+++ private/calculate.js
@@ -0,0 +1,10 @@
+exports.getDamage = (source, data) => {
+    totalDamage = {
+        aaDamage : [100],
+        qDamage : [100],
+        wDamage : [100],
+        eDamage : [100],
+        rDamage : [100]
+    }
+    return totalDamage
+}
