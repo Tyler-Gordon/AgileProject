a959f9ab09c3a64fc5389acef651494a303bba48
Tyler-Gordon <tylerjg1042@gmail.com>
2019-05-02 15:25:02 -0700

    Adjusted front page ui
    Co-Authored-By: andrewyuneeck <andrewyuneeck@users.noreply.github.com>

--- public/js/App.js
+++ public/js/App.js
@@ -34,32 +34,29 @@ class App extends React.Component{
     render () {
         if (this.state.choosing){
             return(  
-                // <section className={"hero is-full"} id='HeroBanner'>
-                //     <div className={"hero-body"}>
-                //         <span className={"section"}>
-                //             <h1 className={"title container is-size-1 has-text-dark has-text-center level"}>
-                //                 Lolpad
-                //                 <img src='../images/logo2.png' width='100px' height='100px'></img>
-                //             </h1>
-                //         </span>
+                <div id='testdiv' className='column'>
+                    <br></br>
+                    <br></br>
 
-                //         <div className='container'>
-                //             <input className='ChampionInput input' type="text" id="userInput" onFocus={()=>{ShowChampionContainer()}} onKeyUp={()=>{search()}}></input>
-                //             <br></br>
-                //             <ChampionList id='AppCentering' onMouseOver={this.onMouseOver.bind(this)} onClick={this.onClick.bind(this)} champions={this.state.champions}/>
-                //         </div>
-                //     </div>
-                // </section>
-
-                <div id='testdiv'>
-                    <h1 id="title">LoLPad<br></br></h1>
-                    <h3>We offer you a calculator that calculates the damage of each champion's abilities.</h3>
+                    <h1 id="title">Lolpad<br></br></h1>
+                    {/* <h3>We offer you a calculator that calculates the damage of each champion's abilities.</h3>
                     <h3>Simply choose the champion, the levels, items, and runes, and you can manage your</h3>
                     <h3>game play better. We want to provide easy operation so you can get your results as</h3>
-                    <h3>fast as you can.</h3>
-                    <h5>{this.state.selectedChampion}<br></br></h5><br></br>
-                    <input placeholder='Select a champion...' className='ChampionInput input' type="text" id="userInput" onFocus={()=>{ShowChampionContainer()}} onKeyUp={()=>{search()}}></input>
-                    <ChampionList id='AppCentering' onMouseOver={this.onMouseOver.bind(this)} onClick={this.onClick.bind(this)} champions={this.state.champions}/>
+                    <h3>fast as you can.</h3> */}
+                    <br></br>
+                    <br></br>
+
+                    <h3 id='subtitle'>
+                        Intuitive Damage Calculator
+                    </h3>
+                    <br></br>
+                    <br></br>
+
+                    <input placeholder='Select a champion...' className='input is-large' type="text" id="userInput" onFocus={()=>{ShowChampionContainer()}} onKeyUp={()=>{search()}}></input>
+                    <div>
+                        <br></br>
+                        <ChampionList id='AppCentering' onMouseOver={this.onMouseOver.bind(this)} onClick={this.onClick.bind(this)} champions={this.state.champions}/>
+                    </div>
                 </div>
 
             )
