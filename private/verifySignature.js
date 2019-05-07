f71fb99b2260c10149931a43289c31383ef66d1a
Connor Vinchoff ConnorVin <43933461+ConnorVin@users.noreply.github.com>
2019-05-07 10:47:16 -0700

    Used Buffers for verifySignature.js

--- private/verifySignature.js
+++ private/verifySignature.js
@@ -1,9 +1,15 @@
 const crypto = require('crypto');
 
 exports.verifySecret = (reqBody, reqHeader) => {
-    let signature = 'sha1' + crypto.createHmac('sha1', process.env.SECRET_TOKEN)
+    
+    let signature = 'sha1=' + crypto.createHmac('sha1', 'process.env.SECRET_TOKEN')
     .update(reqBody)
     .digest('hex');
 
-    return crypto.timingSafeEqual(signature, reqHeader['x-hub-signature']);
+    let incomingHMAC = reqHeader['x-hub-signature'];
+
+    let a = Buffer.alloc(Buffer.byteLength(signature), signature);
+    let b = Buffer.alloc(Buffer.byteLength(incomingHMAC), incomingHMAC);
+
+    return crypto.timingSafeEqual(a, b);
 }
