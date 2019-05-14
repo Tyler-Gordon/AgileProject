4c91fa787b09b18b4100b78c17549606ded586c3
Ramon Lin <yjlin08@gmail.com>
2019-05-14 14:29:36 -0700

    Item choices updating champion stats

--- public/js/EnemyChampion.js
+++ public/js/EnemyChampion.js
@@ -25,14 +25,14 @@ class EnemyChampion extends React.Component {
 
                         <BoughtItems items={this.props.items}  />
 
-                        <div className='column is-one-fifths'>
+                        <div className='column is-one-fifth'>
                             <div>
-                                <img src={this.props.selectedChampion.icon} width={'70%'} height={'70%'}></img>
+                                <img src={this.props.selectedChampion.icon} width='175px' height='175px' />
                             </div>
                             <input className='input' id='PlayerLevel' type='number' name="quantity" value={this.state.level} min="1" max="18" onChange={this.handleChange} />
                         </div>
 
-                        <div className='column is-one-fifths'>
+                        <div className='column is-one-fifth'>
                             <h1 id='ChampionName'>{this.props.selectedChampion.name}</h1>
                         </div>
                     </div>
