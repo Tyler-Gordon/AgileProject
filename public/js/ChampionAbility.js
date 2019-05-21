d0525fe43c141aa2c5fb2e86b4d5bedb06859b3a
Ramon Lin <yjlin08@gmail.com>
2019-05-21 13:59:57 -0700

    Removed levelup icons for autoattack

--- public/js/ChampionAbility.js
+++ public/js/ChampionAbility.js
@@ -4,9 +4,9 @@ class ChampionAbility extends React.Component {
             <div className='column has-text-centered'>
                 <h4>{this.props.ability ? this.props.level : 'Auto'}</h4>
                     <figure>
-                        <img src='../images/AbilityLevelUp.png' id='AbilityUp' onClick={()=>{this.props.levelUp(this.props.ability)}} width='64px' />
+                        <img src='../images/AbilityLevelUp.png' id='AbilityUp' className={this.props.ability ? null : 'is-invisible'} onClick={()=>{this.props.levelUp(this.props.ability)}} width='64px' />
                         <img src={this.props.icon} width='64px' />
-                        <img src='../images/AbilityLevelDown.png' id='AbilityDown' onClick={()=>{this.props.levelDown(this.props.ability)}} width='64px' />
+                        <img src='../images/AbilityLevelDown.png' id='AbilityDown' className={this.props.ability ? null : 'is-invisible'} onClick={()=>{this.props.levelDown(this.props.ability)}} width='64px' />
                     </figure>
                 <h3 id='AbilityStats' >{this.props.damage}</h3>
                 <h3 id='AbilityStats' >{this.props.type}</h3>
