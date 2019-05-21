fd170e789c53aa605907199b07afcbdf3b6b0599
Ramon Lin <yjlin08@gmail.com>
2019-05-20 21:15:49 -0700

    Merged with upstream

--- public/js/Item.js
+++ public/js/Item.js
@@ -3,11 +3,7 @@ class Item extends React.Component {
         return (
             <li id='ItemListItem' onClick={()=>{this.props.chooseItem(this.props.item,this.props.number),this.props.handleItemChosen()}} className='list-item'>
                 <a>
-<<<<<<< HEAD
                     <img className='GameItem' src={this.props.item.icon} width='50px' height='50px'></img>
-=======
-                    <img id='Item' src={this.props.item.icon} width='50px' height='50px'></img>
->>>>>>> upstream/master
                     <h5>{this.props.item.name}</h5>
                 </a>
             </li>
